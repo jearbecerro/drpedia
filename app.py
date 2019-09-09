3918164b76ad66f5f234a6c2b5e0f5d4f9ef643a
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-09-09 23:07:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #Python libraries that we need to import for our bot
 import random
 from flask import Flask, request
-from pymessenger2.bot import Bot
+from pymessenger.bot import Bot
 import os
 
 app = Flask(__name__)
