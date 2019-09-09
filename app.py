112fbc132646c7003799180dcc450c0302a81abe
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-09-09 23:45:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def get_message():
 #uses PyMessenger to send response to user
 def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
-    bot.send_text(recipient_id, response)
+    bot.send_text_message(recipient_id, response)
     return "success"
 
 if __name__ == "__main__":
-    app.run(port=5002, debug=True)
+    app.run()
