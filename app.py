78c9811b5a7385bada408bcdba120f9c3e2958fc
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-09-09 22:25:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,6 +71,17 @@ def send_message(recipient_id, message_text):
         log(r.status_code)
         log(r.text)
         
+def log(msg, *args, **kwargs):  # simple wrapper for logging to stdout on heroku
+    try:
+        if type(msg) is dict:
+            msg = json.dumps(msg)
+        else:
+            msg = unicode(msg).format(*args, **kwargs)
+        print u"{}: {}".format(datetime.now(), msg)
+    except UnicodeEncodeError:
+        pass  # squash logging errors in case of non-ascii text
+    sys.stdout.flush()
+        
 #uses PyMessenger to send response to user
 #def send_message(recipient_id, response):
     #sends user the text message provided via input response parameter
