c390751310d45ad9253fd4b6f3145486967da132
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-09-24 06:30:53 +0300

    Update bot.py

--- messnger_syntax/bot.py
+++ messnger_syntax/bot.py
@@ -342,7 +342,8 @@ class Bot:
               }
             }
         """
-        request_endpoint = '{0}/me/messenger_profile'.format(self.graph_url)
+        #request_endpoint = '{0}/me/messenger_profile'.format(self.graph_url)
+        request_endpoint = '{0}/me/messenger_profile?access_token={1}'.format(self.graph_url,self.access_token)
         response = requests.post(
             request_endpoint,
             params = self.auth_args,
