8bdfa2b3f9946055f469f7d48e79fa941f3adc2d
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-09-29 00:10:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,10 +50,11 @@ def received_qr(event):
     sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
     recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
     text = event["message"]["quick_reply"]["payload"]
-    
+    #2.1
     if text=='physical':
         bot.send_text_message(sender_id,'Physical Infection Quick reply tapped.')
-        
+    
+    #2.2    
     if text=='mental':
         quick_replies = {
                             "content_type":"text",
@@ -65,16 +66,81 @@ def received_qr(event):
                             "payload":"not_diagnosed_mental"
                           }
         bot.send_quick_replies_message(sender_id, 'Is the patient already diagnosed by a real doctor?', quick_replies)
+    #2.2.1
     if text=='yes_diagnosed_mental':
-        yes_diagnosed_mental = {
-                            "content_type":"text",
-                            "title":"Coaching",
-                            "payload":"coaching",
-                            "image_url":image_url+"symptom_checker.png"
-                          }
+         yes_diagnosed_mental = [
+                        {
+                        "type": "postback",
+                        "title": "Coaching",
+                        "payload": "coaching"
+                        }
+                        ]
         bot.send_text_message(sender_id,"So, you want to know how to handle the patient's with mental disorder?")
-        bot.send_quick_replies_message(sender_id, "Just tap 'Coaching' ", yes_diagnosed_mental)
-    if text=='coaching':
+        bot.send_button_message(sender_id, "Just tap 'Coaching' ", yes_diagnosed_mental)
+        #proceed to payload button if payload=='coaching'
+    #2.2.2    
+    if text=='not_diagnosed_mental':
+        not_diagnosed_mental = [
+                        {
+                        "type": "postback",
+                        "title": "Symptom Checker",
+                        "payload": "mental_symptom_checker"
+                        }
+                        ]
+        bot.send_text_message(sender_id,"So, you want to know what is you/your child's mental health problem?")
+        bot.send_button_message(sender_id, "Just tap 'Symptom Checker'", not_diagnosed_mental)
+        #proceed to payload button if payload=='mental_symptom_checker'
+
+#if user send a message in text
+def received_text(event):
+    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
+    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+    text = event["message"]["text"]
+    #2.2.1.1..{
+    if text.lower() in ("attention deficit hyperactivity disorder", "adhd"):#if user send text 'adhd'
+        choose_option_mental('send_tips_adhd','check_adhd','ADHD')
+        #proceed to payload button if payload=='send_tips_adhd' or if payload=='check_adhd'
+    
+    if text.lower() in ("oppositional defiant disorder", "odd"):
+        choose_option_mental('send_tips_odd','check_odd','ODD')
+        #proceed to payload button if payload=='send_tips_odd' or if payload=='check_odd'
+        
+    if text.lower() in ("autism spectrum disorder", "asd", "autism"):
+        choose_option_mental('send_tips_asd','check_asd','Autism Spectrum Disorder')
+        #proceed to payload button if payload=='send_tips_asd' or if payload=='check_asd'
+        
+    if text.lower() in ("anxiety disorder", "anxiety","ad"):
+        choose_option_mental('send_tips_ad','check_ad','Anxiety Disorder')
+        #proceed to payload button if payload=='send_tips_ad' or if payload=='check_ad'
+        
+    if text.lower() in ("depression", "depression disorder","depress"):
+        choose_option_mental('send_tips_d','check_d','Depression')
+        #proceed to payload button if payload=='send_tips_d' or if payload=='check_d'
+        
+    if text.lower() in ("bipolar disorder", "bipolar","bd"):
+        choose_option_mental('send_tips_bd','check_bd','Bipolar Disorder')
+        #proceed to payload button if payload=='send_tips_bd' or if payload=='check_bd' 
+        
+    if text.lower() in ("learning disorders", "learning","ld"):
+        choose_option_mental('send_tips_ld','check_ld','Learning Disorder')
+        #proceed to payload button if payload=='send_tips_ld' or if payload=='check_ld' 
+        
+    if text.lower() in ("conduct disorders", "conduct","cd"):
+        choose_option_mental('send_tips_cd','check_cd', 'Conduct Disorder')
+        #proceed to payload button if payload=='send_tips_cd' or if payload=='check_cd' 
+    #2.2.1.1..}   
+        
+    if text.lower()=='about':
+        bot.send_text_message(sender_id,'Intruction on how to user this chatbot under development')
+
+#if user tap a button from a regular button
+def received_postback(event):
+    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
+    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+    payload = event["postback"]["payload"]
+    
+    #2.2.1.1{
+    if payload=='coaching':
         bot.send_text_message(sender_id,'What is the diagnosed mental disorder of a patient from a real Doctor.')
         bot.send_text_message(sender_id,'These are the following mental disorders we can cater:')
         '''attention deficit hyperactivity disorder (ADHD)
@@ -88,59 +154,15 @@ def received_qr(event):
         bot.send_text_message(sender_id,'Attention Deficit Hyperactivity Disorder (ADHD)\nOppositional Defiant Disorder (ODD)\nAutism Spectrum Disorder (ASD)\nAnxiety Disorder\nDepression\nBipolar Disorder\nLearning Disorders\nConduct Disorders')
         bot.send_text_message(sender_id,'Just type ADHD, ODD, ASD/Autism, Anxiety, Depression, Bipolar, Learning or Conduct.')
         bot.send_text_message(sender_id,'If your mental concern is not in the list,\nwe are very sorry to inform you that we cannot cater your concern.')
-      
-    if text=='not_diagnosed_mental':
-        bot.send_text_message(sender_id,"So, you did not know what is the patient's mental disorder.")
-        not_diagnosed_mental = {
-                            "content_type":"text",
-                            "title":"Symptom Checker",
-                            "payload":"mental_symptom_checker",
-                            "image_url":image_url+"symptom_checker.png"
-                          }
-        bot.send_quick_replies_message(sender_id, "Just tap 'Symptom Checker'", not_diagnosed_mental)
-    if text=='mental_symptom_checker':
+    if payload=='check_adhd':
+        bot.send_text_message(sender_id,'Attention deficit hyperactivity disorder (ADHD) is a mental health disorder that can cause above-normal levels of hyperactive and impulsive behaviors.\nPeople with ADHD may also have trouble focusing their attention on a single task or sitting still for long periods of time.')
+        
+    #2.2.2.1}
+    if payload=='mental_symptom_checker':
         bot.send_text_message(sender_id,"symptom checker")
-
-#if user send a message in text
-def received_text(event):
-    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
-    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-    text = event["message"]["text"]
-    
-    if text.lower() in ("attention deficit hyperactivity disorder", "adhd"):#if user send text 'adhd'
-        bot.send_text_message(sender_id,'Attention deficit hyperactivity disorder (ADHD) is a mental health disorder that can cause above-normal levels of hyperactive and impulsive behaviors.\nPeople with ADHD may also have trouble focusing their attention on a single task or sitting still for long periods of time.')   
         
-    if text.lower() in ("oppositional defiant disorder", "odd"):
-        bot.send_text_message(sender_id,'oppositional defiant disorder') 
         
-    if text.lower() in ("autism spectrum disorder", "asd", "autism"):
-        bot.send_text_message(sender_id,'autism')   
-        
-    if text.lower() in ("anxiety disorder", "anxiety","ad"):
-        bot.send_text_message(sender_id,'anxiety disorder') 
-        
-    if text.lower() in ("depression", "depression disorder","depress"):
-        bot.send_text_message(sender_id,'depression') 
-        
-    if text.lower() in ("bipolar disorder", "bipolar","bd"):
-        bot.send_text_message(sender_id,'bipolar disorder')     
-        
-    if text.lower() in ("learning disorders", "learning","ld"):
-        bot.send_text_message(sender_id,'learning disorders')  
-        
-    if text.lower() in ("conduct disorders", "conduct","cd"):
-        bot.send_text_message(sender_id,'conduct disorders') 
-        
-    if text.lower()=='about':
-        bot.send_text_message(sender_id,'Intruction on how to user this chatbot under development')
-
-#if user tap a button from a regular button
-def received_postback(event):
-    sender_id = event["sender"]["id"]        # the facebook ID of the person sending you the message
-    recipient_id = event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-    payload = event["postback"]["payload"]
-    
-    
+    #Get started button tapped{
     if payload=='start':
         send_choose_concern(sender_id)
     #Persistent Menu Buttons        
@@ -152,64 +174,24 @@ def received_postback(event):
         bot.send_text_message(sender_id,'About Under Construction')
     if payload=='adhd':
         bot.send_text_message(sender_id,'About Under Construction')
-        
-def send_behavioral(sender_id):
-    buttons = [
+    #}
+#2.2.1.1 use multipe times
+def choose_option_mental(payload1,payload2,name):
+    confirm = [
                         {
                         "type": "postback",
-                        "title": "ADHD",
-                        "payload": "adhd"
+                        "title": "Send Coaching Tips",
+                        "payload": payload1
                         },{
                         "type": "postback",
-                        "title": "Autism",
-                        "payload": "autism"
-                        },{
-                        "type": "postback",
-                        "title": "ODD",
-                        "payload": "odd"
+                        "title": "Check",
+                        "payload": payload2
                         }
                         ]
-    
-    buttons2 = [        {
-                        "type": "postback",
-                        "title": "Anxiety Disorder",
-                        "payload": "anxiety"
-                        },{
-                        "type": "postback",
-                        "title": "Depression",
-                        "payload": "depression"
-                        }
-                    ]
-    buttons3 = [
-                       {
-                        "type": "postback",
-                        "title": "Bipolar Disorder",
-                        "payload": "bipolar"
-                        },{
-                        "type": "postback",
-                        "title": "Learning Disorders",
-                        "payload": "learning"
-                        },{
-                        "type": "postback",
-                        "title": "Conduct Disorders",
-                        "payload": "Conduct"
-                        }
-                    ] 
-    dont_know =[ {
-                        "type": "postback",
-                        "title": "Symptom Checker",
-                        "payload": "b_checker"
-                          }]
-    bot.send_text_message(sender_id,'What is your behavioral concern?')
-    send_button(sender_id,'Behavioral Disorders',buttons)
-    send_button(sender_id,'Emotional Disorders',buttons2)
-    send_button(sender_id,'Developmental Disorders',buttons3)
-    send_button(sender_id,"Don't know yet?",dont_know)
-    
-def send_button(sender_id,text,buttons):
-    bot.send_button_message(sender_id,text, buttons)
-    return "success"
-
+    bot.send_text_message(sender_id,"With tapping 'Send Coaching Tips'.\nYou already know that you/your child or the patient had{}".format(name))
+    bot.send_text_message(sender_id,"To check if you/your child or the patient has {}.\nTap 'Check'".format(name))
+    bot.send_button_message(sender_id, "Choose:", confirm)
+#1   
 def send_choose_concern(sender_id):
     bot.send_text_message(sender_id, "Hi I'm DrPedia\nI'm here to cater your pediatric concern.")
     bot.send_text_message(sender_id, "For that you'll have to answer a few questions about your concerns.")
