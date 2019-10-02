9ac13916f4bc102dcce687e36936adf71595e76a
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-10-03 01:00:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -121,37 +121,40 @@ def received_text(event):
         choose_option_mental(sender_id,'send_tips_adhd','check_adhd','ADHD')
         #proceed to payload button if payload=='send_tips_adhd' or if payload=='check_adhd'
     
-    if text.lower() in ("oppositional defiant disorder", "odd"):
+    elif text.lower() in ("oppositional defiant disorder", "odd"):
         choose_option_mental(sender_id,'send_tips_odd','check_odd','ODD')
         #proceed to payload button if payload=='send_tips_odd' or if payload=='check_odd'
         
-    if text.lower() in ("autism spectrum disorder", "asd", "autism"):
+    elif text.lower() in ("autism spectrum disorder", "asd", "autism"):
         choose_option_mental(sender_id,'send_tips_asd','check_asd','Autism Spectrum Disorder')
         #proceed to payload button if payload=='send_tips_asd' or if payload=='check_asd'
         
-    if text.lower() in ("anxiety disorder", "anxiety","ad"):
+    elif text.lower() in ("anxiety disorder", "anxiety","ad"):
         choose_option_mental(sender_id,'send_tips_ad','check_ad','Anxiety Disorder')
         #proceed to payload button if payload=='send_tips_ad' or if payload=='check_ad'
         
-    if text.lower() in ("depression", "depression disorder","depress"):
+    elif text.lower() in ("depression", "depression disorder","depress"):
         choose_option_mental(sender_id,'send_tips_d','check_d','Depression')
         #proceed to payload button if payload=='send_tips_d' or if payload=='check_d'
         
-    if text.lower() in ("bipolar disorder", "bipolar","bd"):
+    elif text.lower() in ("bipolar disorder", "bipolar","bd"):
         choose_option_mental(sender_id,'send_tips_bd','check_bd','Bipolar Disorder')
         #proceed to payload button if payload=='send_tips_bd' or if payload=='check_bd' 
         
-    if text.lower() in ("learning disorders", "learning","ld"):
+    elif text.lower() in ("learning disorders", "learning","ld"):
         choose_option_mental(sender_id,'send_tips_ld','check_ld','Learning Disorder')
         #proceed to payload button if payload=='send_tips_ld' or if payload=='check_ld' 
         
-    if text.lower() in ("conduct disorders", "conduct","cd"):
+    elif text.lower() in ("conduct disorders", "conduct","cd"):
         choose_option_mental(sender_id,'send_tips_cd','check_cd', 'Conduct Disorder')
         #proceed to payload button if payload=='send_tips_cd' or if payload=='check_cd' 
     #2.2.1.1..}   
         
-    if text.lower()=='about':
+    elif text.lower()=='about':
         bot.send_text_message(sender_id,'Intruction on how to user this chatbot under development')
+   
+    else:
+        bot.send_text_message(sender_id,'Humans are so complicated Im not train to understain stupid things')
 
 #if user tap a button from a regular button
 def received_postback(event):
