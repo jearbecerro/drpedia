067da27f3bb1e314f889aad503b9715f9e126e74
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-10-25 01:32:28 +0800

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -46,7 +46,7 @@ def create_user(users, user_id, user_fb):
 def set_terms(users, sender_id):
     users.update({"user_id": sender_id},{"$set":{"accept_disclaimer": "Yes"}})
 def get_terms(users, sender_id):
-    return users.find_one({'user_id': sender_id})['accept_disclaimer']
+    return users.find_one({'user_id': sender_id,'accept_disclaimer'})
 
 #Setter Getter for last message send by the DrPedia ---
 #set last message ask by the chatbot
