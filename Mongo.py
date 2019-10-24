c7b135e0d10a161709c328dd1fe1c8bc9daad20a
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-10-25 03:05:34 +0800

    Update Mongo.py

--- Mongo.py
+++ Mongo.py
@@ -46,15 +46,15 @@ def create_user(users, user_id, user_fb):
 def set_terms(users, sender_id):
     users.update({"user_id": sender_id},{"$set":{"accept_disclaimer": "Yes"}})
 def get_terms(users, sender_id):
-    return users.find({'user_id': sender_id},{'accept_disclaimer':1,'_id':0})
-
+    a = users.find_one({'user_id': sender_id},{'accept_disclaimer':1,'_id':0})
+    return a['accept_disclaimer']
 #Setter Getter for last message send by the DrPedia ---
 #set last message ask by the chatbot
 def set_ask(users, sender_id, ask):
     users.update({"user_id": sender_id},{"$set":{"last_message_ask": ask}})
 #get last message ask by the chatbot
 def get_ask(users, sender_id):
-    return users.find({'user_id': sender_id},{'last_message_ask':1,'_id':0})
+    return users.find_one({'user_id': sender_id},{'last_message_ask':1,'_id':0})
 #End Setter Getter last message send by the DrPedia ---
 
 #Setter Getter for last message send by the user ---
@@ -63,7 +63,7 @@ def set_answer(users, sender_id, answer):
     users.update({"user_id": sender_id},{"$set":{"last_message_answer": answer}})
 #get last message ask by the chatbot
 def get_answer(users, sender_id):
-    return users.find({'user_id': sender_id},{'last_message_answer':1,'_id':0})
+    return users.find_one({'user_id': sender_id},{'last_message_answer':1,'_id':0})
 #End Setter Getter last message send by the user ---
 
 # Input: Facebook's sender_id
