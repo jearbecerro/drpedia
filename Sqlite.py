3d62e5088ff1d2802f4180b537be172af8895d73
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-10-27 18:52:03 +0800

    Update Sqlite.py

--- Sqlite.py
+++ Sqlite.py
@@ -36,30 +36,37 @@ def user_exists(sender_id):
     
 def create_user(sender_id, user_fb):
     timestamp = datetime.strftime(datetime.now(),"%Y-%m-%d %H:%M:%S")
-    qry="insert into users (sender_id, first_name) values('Becerro','Aljie Rose');"
+    qry="insert into users (sender_id, created_at, last_seen, first_name, last_name, last_message_ask, last_message_answer, accept_disclaimer) values({},{},{},{},{},{},{},{});".format(sender_id, timestamp, "1970-01-01 00:00:00", user_fb['first_name'], user_fb['last_name'], 'None','None','No')
     cur.execute(qry)
     db.commit() 
-    user_insert = {'user_id': sender_id, 
-                    'created_at': timestamp,
-                    'last_seen': "1970-01-01 00:00:00",
-                    'first_name':user_fb['first_name'],
-                    'last_name':user_fb['last_name'],
-                    'last_message_ask':'None',
-                    'last_message_answer':'None',
-                    'accept_disclaimer':'No'
-                   }
-    users.insert(user_insert)    
     
+def set_terms(sender_id):
+    cur.execute('UPDATE users SET accept_disclaimer = {} where sender_id = {}'.format('Yes',sender_id))
+    con.commit()
+def get_terms(sender_id):
+    cur.execute("SELECT accept_disclaimer from users where sender_id = {}".format(sender_id))
+    result = cur.fetchone()            
+    return result
 
+#Setter Getter for last message send by the DrPedia ---
+    #set last message ask by the chatbot
+def set_ask(sender_id, ask):
+    cur.execute('UPDATE users SET last_message_ask = {} where sender_id = {}'.format(ask, sender_id))
+    con.commit()
+def get_ask(sender_id):
+    cur.execute("SELECT last_message_ask from users where sender_id = {}".format(sender_id))
+    result = cur.fetchone()            
+    return result
+#End Setter Getter last message send by the DrPedia ---
+#Setter Getter for last message send by the user ---
+#set last message ask by the chatbot
+def set_answer(users, sender_id, answer):
+    cur.execute('UPDATE users SET last_message_answer = {} where sender_id = {}'.format(answer, sender_id))
+    con.commit()
+#get last message ask by the chatbot
+def get_answer(users, sender_id):
+    cur.execute("SELECT last_message_answer from users where sender_id = {}".format(sender_id))
+    result = cur.fetchone()            
+    return result
+#End Setter Getter last message send by the user ---
 
-''''
-qry="insert into users (last_name, first_name) values('Becerro','Aljie Rose');"
-cur=db.cursor()
-cur.execute(qry)
-db.commit() 
-
-sql="SELECT first_name from users where last_name = 'Becerro';"
-cur.execute(sql)
-record=cur.fetchone()
-print (record)
-db.close()'''
