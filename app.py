f4a27f2f50f3a1589262de43af681b8e539c9be6
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-10-27 17:01:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -122,6 +122,8 @@ def received_text(event):
         choose_option_mental(sender_id,'send_tips_cd','check_cd', 'Conduct Disorder')
         #proceed to payload button if payload=='send_tips_cd' or if payload=='check_cd' 
     #end Mental Health}
+    else:
+        bot.send_text_message(sender_id,'Humans are so complicated Im not train to understand things well. Sorry :(')
     '''
     elif Mongo.get_ask(users,sender_id) == 'pleased to meet me?' and Mongo.get_answer(users,sender_id) =='None':
         button = [
@@ -132,8 +134,7 @@ def received_text(event):
                             }
                             ]
         bot.send_button_message(sender_id, 'Your not happy to meet me {}?'.format(first_name(sender_id)), button) '''
-    else:
-        bot.send_text_message(sender_id,'Humans are so complicated Im not train to understand things well. Sorry :(')
+    
         
         
 def greet_disclaimer(sender_id):
