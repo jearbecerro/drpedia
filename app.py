3944a266c5121eebf5841c9ce9f7148919aaa440
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-10-31 16:48:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -119,8 +119,7 @@ def received_text(event):
                             ]
         bot.send_button_message(sender_id, 'Your not happy to meet me {} ?'.format(first_name(sender_id)), button) 
         #bot.send_text_message(sender_id, ' ASDA' +Sqlite.get_ask(sender_id))'''
-    else:
-        bot.send_text_message(sender_id,'Humans are so complicated {} Im not trained to understand things well. Sorry :('.format(first_name(sender_id)))
+    bot.send_text_message(sender_id,'Humans are so complicated {} Im not trained to understand things well. Sorry :('.format(first_name(sender_id)))
         
         
         
