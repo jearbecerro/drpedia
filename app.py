05744a024a7a89359b4139a41d23cc6abe6df3ce
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-11-03 02:36:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def receive_message():
                     ask = user_data['last_message_ask']
                     asnwer = user_data['last_message_answer']
                     terms = user_data['accept_disclamer']
-                  
+                print(create_at+last_seen+lname)  
                 elif message['message'].get('text'):
                     if message['message'].get('quick_reply'):
                         received_qr(message)  
