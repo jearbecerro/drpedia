db9fc118d14c31be0c83a168e859470bb9abb376
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-11-21 04:34:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ def received_text(event):
     '''
     #Mental Health{
     if text.lower() in ("attention deficit hyperactivity disorder", "adhd"):#if user send text 'adhd'
-        choose_howto(sender_id,'remedies_adhd','medication_adhd','about_adhd','ADHD'):
+        choose_howto(sender_id,'remedies_adhd','medication_adhd','about_adhd','ADHD')
         #choose_option_mental(sender_id,'send_tips_adhd','check_adhd','ADHD')
         #proceed to payload button if payload=='send_tips_adhd' or if payload=='check_adhd'
 
