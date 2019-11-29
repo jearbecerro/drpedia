181a7b8d265a467c377c96a91c7e0e8d4f17fd48
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-11-29 14:00:17 +0800

    Update app.py

--- app.py
+++ app.py
@@ -308,8 +308,12 @@ def send_remedies(sender_id,symptoms):
         symptom = symptom.replace("/", "")
         symptom = symptom.replace("-", "")
         symptom = symptom.replace(",", "")
+        the_rest_symptoms = [i for i in illness if i not in patient_symptoms]
+        for tr_symptom in the_rest_symptoms:
+            res = [ tr_symptom[0],tr_symptom[-1] ] 
         if len(symptom) > 1:
             element.append({"title":symptom.capitalize(),"image_url":image_url +symptom.lower()+'.png',"subtitle":"","default_action": {"type": "postback","payload":"","webview_height_ratio": "tall",},"buttons":[{"type":"postback","title":"Send Remedies","payload":symptom+'_remedies'}] },) 
+            
         else:
             element.append({"title":symptom.capitalize(),"image_url":image_url +symptom.lower()+'.png',"subtitle":"","default_action": {"type": "postback","payload":"","webview_height_ratio": "tall",},"buttons":[{"type":"postback","title":"Send Remedies","payload":symptom+'_remedies'}]})             
         bot.send_generic_message(sender_id, element)
@@ -333,20 +337,19 @@ def get_the_rest_symptoms(patient,sender_id,text, patient_symptoms,illness,total
             the_rest_symptoms = [i for i in illness if i not in patient_symptoms]
             Mongo.set_patient(patient, sender_id, 'count_yes', count_yes +1)
             Mongo.set_patient(patient, sender_id, 'total_symptoms', total_symptoms+1)
-            for tr_symptom in the_rest_symptoms:
-                res = [ tr_symptom[0],tr_symptom[-1] ] 
-                if total_illness_symptoms == total_symptoms and res[0] == None:
-                    if get_average(count_yes, total_symptoms) >= 50:
-                        Mongo.set_patient(patient, sender_id, 'count_yes', 0)
-                        Mongo.set_patient(patient, sender_id, 'total_symptoms', 0)
-                        bot.send_text_message(sender_id, "Base on my symptom checker the {} might have chance of having {}.".format(phrase2,ill_name))
-                        bot.send_text_message(sender_id, "I suggest that you must get a doctors consultation urgently!")
-                        send_remedies(sender_id,ill_name)
-                        break
-                else:   
-                    Mongo.set_patient(patient, sender_id, 'symptoms',"{}{},".format(patient_symptoms,str(res[0])))
-                    twoqrbtn = {"content_type":"text","title":"Yes","payload":'yes_'+res[0]},{"content_type":"text","title":"No","payload":'no_+res[0]'}
-                    bot.send_quick_replies_message(sender_id, '{} experiencing {}?'.format(phrase,res[0]), twoqrbtn)          
+            res = [ tr_symptom[0],tr_symptom[-1] ] 
+            if total_illness_symptoms == total_symptoms and res[0] == None:
+                if get_average(count_yes, total_symptoms) >= 50:
+                    Mongo.set_patient(patient, sender_id, 'count_yes', 0)
+                    Mongo.set_patient(patient, sender_id, 'total_symptoms', 0)
+                    bot.send_text_message(sender_id, "Base on my symptom checker the {} might have chance of having {}.".format(phrase2,ill_name))
+                    bot.send_text_message(sender_id, "I suggest that you must get a doctors consultation urgently!")
+                    send_remedies(sender_id,ill_name)
+                    
+            else:   
+                Mongo.set_patient(patient, sender_id, 'symptoms',"{}{},".format(patient_symptoms,str(res[0])))
+                twoqrbtn = {"content_type":"text","title":"Yes","payload":'yes_'+res[0]},{"content_type":"text","title":"No","payload":'no_+res[0]'}
+                bot.send_quick_replies_message(sender_id, '{} experiencing {}?'.format(phrase,res[0]), twoqrbtn)          
         if text =='yes_'+res[0]:
             Mongo.set_patient(patient, sender_id, 'count_yes', count_yes +1)
             twoqrbtn = {"content_type":"text","title":"Yes","payload":'yes_'+res[0]},{"content_type":"text","title":"No","payload":'no_+res[0]'}
