36231cf3699710939024038d7f0a1fcc122f1c9a
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-11-29 16:05:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -304,15 +304,15 @@ def countOccurrence(tup, lst):
 def send_remedies(sender_id,symptoms,illness):
     patient_symptoms = list(symptoms.split(",")) 
     element = []
-    tr_symptom = [i for i in illness if i not in patient_symptoms]
+    
     for symptom in patient_symptoms:
         symptom = symptom.replace(" ", "")
         symptom = symptom.replace("/", "")
         symptom = symptom.replace("-", "")
         symptom = symptom.replace(",", "")
-        
-        res = [ tr_symptom[0],tr_symptom[-1] ] 
-        res[0]
+        the_rest_symptom = [i for i in illness if i not in patient_symptoms]
+        for tr_symptom in the_rest_symptom:
+            res = [ tr_symptom[0],tr_symptom[-1] ] 
         if len(symptom) > 1:
             element.append([{"title":res[0].capitalize(),"image_url":image_url +res[0].lower()+'.png',"subtitle":"","default_action": {"type": "postback","payload":"","webview_height_ratio": "tall",},"buttons":[{"type":"postback","title":"Send Remedies","payload":res[0]+'_remedies'}] }]) 
         else:
@@ -328,7 +328,8 @@ def get_the_rest_symptoms(patient,sender_id,text, patient_symptoms,illness,total
             Mongo.set_patient(patient,sender_id,'count_yes',total_has_symptoms)
             Mongo.set_patient(patient, sender_id, 'total_symptoms', total_has_symptoms)
             #Mongo.set_patient(patient,sender_id,'total_symptoms',total_symptoms)
-            res = [ tr_symptom[0],tr_symptom[-1] ] 
+            for tr_symptom in the_rest_symptom:
+                res = [ tr_symptom[0],tr_symptom[-1] ] 
             Mongo.set_patient(patient, sender_id, 'symptoms',"{}{},".format(patient_symptoms,str(res[0])))
             twoqrbtn = {"content_type":"text","title":"Yes","payload":'yes_'+res[0]},{"content_type":"text","title":"No","payload":'no_+res[0]'}
             bot.send_quick_replies_message(sender_id, '{} experiencing {}?'.format(phrase,res[0]), twoqrbtn)          
@@ -336,7 +337,8 @@ def get_the_rest_symptoms(patient,sender_id,text, patient_symptoms,illness,total
             the_rest_symptoms = [i for i in illness if i not in patient_symptoms]
             Mongo.set_patient(patient, sender_id, 'count_yes', count_yes +1)
             Mongo.set_patient(patient, sender_id, 'total_symptoms', total_symptoms+1)
-            res = [ tr_symptom[0],tr_symptom[-1] ] 
+            for tr_symptom in the_rest_symptom:
+                res = [ tr_symptom[0],tr_symptom[-1] ] 
             if total_illness_symptoms == total_symptoms and res[0] == None:
                 if get_average(count_yes, total_symptoms) >= 50:
                     Mongo.set_patient(patient, sender_id, 'count_yes', 0)
