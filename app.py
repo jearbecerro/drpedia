843dc9d991ffd95a981c6c3b7ec3ebfc27f60ca9
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-12-01 23:06:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -305,18 +305,16 @@ def countOccurrence(tup, lst):
 	return sum(counts[i] for i in lst) 
 
 def send_remedies(sender_id,symptoms):
+	patient_data = Mongo.get_data_patient(patient, sender_id)
 	patient_symptoms = list(symptoms.split(","))
 	len_ps = len(patient_symptoms)
 	element = []
 	if len_ps > 2:
 		print(len_ps)
 		for x in range(0,len_ps-1):
-			patient_data = Mongo.get_data_patient(patient, sender_id)
 			elements = patient_data["elements"]
 			rest = patient_symptoms[x].replace(" ","").replace("/","").replace("-","").replace(",","")
-			element.append({"title":patient_symptoms[x].capitalize(),"image_url":image_url +rest+'.png',"subtitle":"If symptom persist or worsen get a doctor's consultation.","buttons":[{"type":"postback","title":"Remedies","payload":rest+"_remedies"}]},)
-			
-			Mongo.set_patient(patient, sender_id, 'elements', element)
+			Mongo.set_patient(patient, sender_id, 'elements', elements.append({"title":patient_symptoms[x].capitalize(),"image_url":image_url +rest+'.png',"subtitle":"If symptom persist or worsen get a doctor's consultation.","buttons":[{"type":"postback","title":"Remedies","payload":rest+"_remedies"}]},))
 			if x == len_ps-1:
 				bot.send_generic_message(sender_id, elements)
 				break
