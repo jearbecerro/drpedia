4c5c507377f710110e3060453d7d61fc2da3c882
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-12-01 12:46:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -507,7 +507,9 @@ def received_qr(event):
 			
 	if text =='no_symptoms': 
 		bot.send_text_message(sender_id,"No") 
-		send_remedies(sender_id,symptoms)
+		element = [{"title":"Fever","image_url":image_url +'fever.png',"subtitle":"","default_action": {"type": "postback","payload":"","webview_height_ratio": "tall",},"buttons":[{"type":"postback","title":"Send Remedies","payload":'fever_remedies'}]}]        
+		bot.send_generic_message(sender_id, element) 
+		#send_remedies(sender_id,symptoms)
 		
 	if text == 'dengue_remedies':
 		oneqrbtn = [{"content_type":"text","title":"📩Send Another","payload":'send_dengue_remedies'}]
