494710a69947cd8d3854712501a24430d5c5464b
clvrjc2 <54774555+clvrjc2@users.noreply.github.com>
2019-12-01 15:37:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1444,7 +1444,8 @@ def received_postback(event):
                              ]
                         }
                       ]
-		bot.send_generic_message(sender_id, elements) 
+		#bot.send_generic_message(sender_id, elements)
+		send_remedies(sender_id,'fever,')
 		if terms == "Yes":
 			Mongo.set_ask(users,sender_id, "")
 			Mongo.set_answer(users,sender_id, "")
